9bac25cffc60fb2363c09f367bcbb6c60c79a17e
Laura Juiz <juiz.laurai@gmail.com>
2023-09-27 16:28:33 -0300

    Estilado Cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,37 @@
 import { Layout } from '@/components/layouts';
-import { Typography } from '@mui/material';
+import { Grid, Card, CardHeader, CardContent } from '@mui/material';
 
 export default function Home() {
   return (
-    <Layout>
-      <Typography variant='h2' color={'primary'}>Hola Mundo</Typography>
+    <Layout title='Home - Opensource Jira'>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title='Pendientes' sx={{ textAlign: 'center' }} />
+            <CardContent>
+              Probando
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title='En Progreso' sx={{ textAlign: 'center' }} />
+            <CardContent>
+              Probando
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title='Completadas' sx={{ textAlign: 'center' }} />
+            <CardContent>
+              Probando
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
